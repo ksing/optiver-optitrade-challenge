390ed85f41565e6fefd0748dc2551ef5917ec691
ksing <singhalkushagra@gmail.com>
2019-11-25 14:56:00 +0100

    Update prep_strigo.sh

--- prep_strigo.sh
+++ prep_strigo.sh
@@ -2,4 +2,5 @@
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get -y update
 sudo apt-get -y install python3.6
-sudo apt-get -y install python3-pip
+sudo ln -sf /usr/bin/python3.6 /usr/bin/python3
+sudo python3.6 -m pip install -U pip
