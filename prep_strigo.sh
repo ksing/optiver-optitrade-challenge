d456a5ad1f9f0719e3f90f50acccc4e760045856
gkalas8 <49941463+gkalas8@users.noreply.github.com>
2019-11-26 09:50:26 +0100

    Update prep_strigo.sh

--- prep_strigo.sh
+++ prep_strigo.sh
@@ -3,6 +3,6 @@
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get -y update
 sudo apt-get -y install python3.6
-sudo ln -sf /usr/bin/python3.6 /usr/bin/python3
-sudo python3.6 -m pip install -U pip
+#sudo ln -sf /usr/bin/python3.6 /usr/bin/python3
+sudo apt-get -y install python3-pip                                                                                                             
 sudo apt-get -y autoremove
